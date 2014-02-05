5898380cc74dd36462b91a6499b62b27c2240282
MattDekinder <dekinder@ualberta.ca>
2014-02-04 19:13:18 -0700

    lab changes

--- src/ualberta/cmput301/camerademo/CameraDemoActivity.java
+++ src/ualberta/cmput301/camerademo/CameraDemoActivity.java
@@ -1,13 +1,23 @@
 package ualberta.cmput301.camerademo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import ualberta.cmput301.camerodemo.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Activity;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.widget.ImageView.ScaleType;
 import android.widget.TextView;
 
 public class CameraDemoActivity extends Activity {
@@ -26,8 +36,9 @@ public class CameraDemoActivity extends Activity {
 		
 		// Set up the listener
 		OnClickListener listener = new OnClickListener() {
+			@Override
 			public void onClick(View view) {
-				takeAPhoto(); // implement this method
+				takeAPhoto();
 			}
 		};
 		// Register a callback to be invoked when this view is clicked
@@ -40,11 +51,30 @@ public class CameraDemoActivity extends Activity {
 	// finishes, while startActivityForResult() method will. To retrieve the returned result, you may 
 	// need implement onAcitityResult() method.
 	public void takeAPhoto() {
-		// To Do		
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		startActivityForResult(intent, 0);
+		
 	}
 	
+	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		// To Do
+		if (data != null){
+			if (resultCode == RESULT_OK) {
+				Bitmap bm = (Bitmap) data.getExtras().getParcelable("data");
+				imageButton.setImageBitmap(bm);
+				imageButton.setScaleType(ScaleType.FIT_CENTER);
+				String sdcard = Environment.getExternalStorageDirectory().getAbsolutePath();
+	//			data.putExtra( )
+				
+				textView.setText("Photo OK");
+			} 
+			else if (resultCode == RESULT_CANCELED) {
+				textView.setText("Photo CANCELLED");				
+			}
+			else {
+				textView.setText("I have no idea what happened");
+			}
+		}
 	}	
 	
 	@Override
